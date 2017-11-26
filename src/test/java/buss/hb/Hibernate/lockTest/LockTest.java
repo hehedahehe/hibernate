5be7692096dc709a1bfaf031d1017f1c72df0feb
lrb <1246531124@qq.com>
2017-11-26 22:39:13 +0800

    +乐观锁测试

--- src/test/java/buss/hb/Hibernate/lockTest/LockTest.java
+++ src/test/java/buss/hb/Hibernate/lockTest/LockTest.java
@@ -9,6 +9,7 @@ import org.hibernate.StaleObjectStateException;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Random;
 
 /*
  * @desc
@@ -32,6 +33,8 @@ public class LockTest extends HBBaseTest{
         session.close();
     }
 
+    private Random random = new Random();
+
     @Test
     public void testUpdateGrade(){
         Session session = sessionFactory.openSession();
@@ -43,10 +46,11 @@ public class LockTest extends HBBaseTest{
         List<User> userList = query.list();
         User mike = userList.get(0);
         Grade grade = mike.getGrade();
-        grade.setScore(90);
+        grade.setScore(random.nextInt(100));
         session.saveOrUpdate(mike);
         session.getTransaction().commit();
         session.close();
+        System.out.println("");
     }
 
     @Test
@@ -61,15 +65,61 @@ public class LockTest extends HBBaseTest{
             List<User> userList = query.list();
             User mike = userList.get(0);
             Grade grade = mike.getGrade();
-            grade.setScore(0);
+            grade.setScore(random.nextInt(200));
             session.saveOrUpdate(mike);
             session.getTransaction().commit();
             session.close();
         }catch (StaleObjectStateException e){
             System.out.println("已经有人在您之前更新过该条数据，您若要继续更新，请再次进行更新操作。");
         }
+        System.out.println("");
 
     }
+
+
+    @Test
+    public void testVersion1(){
+        String id ="4028b0815f76b7e9015f76b7f0400001";
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        String hql = "from ";
+        hql += "Grade where id=?";
+        Query query = session.createQuery(hql);
+        query.setParameter(0,id);
+        Grade grade = (Grade) query.list().get(0);
+        session.getTransaction().commit();
+        session.close();
+
+        grade.setScore(random.nextInt(200)); //游离态
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(grade);
+
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
+
+    @Test
+    public void testVersion2(){
+        String id ="4028b0815f76b7e9015f76b7f0400001";
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        String hql = "from ";
+        hql += "Grade where id=?";
+        Query query = session.createQuery(hql);
+        query.setParameter(0,id);
+        Grade grade = (Grade) query.list().get(0);
+        grade.setScore(random.nextInt(200));
+        session.update(grade);
+
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
 
 
 }
